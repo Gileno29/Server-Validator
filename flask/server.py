840f0d92322f6e2b1870e9552e897a41dafb900e
Gileno <gileno@tsmx.net.br>
2023-12-01 17:10:45 -0300

    fix(menu): corrige erro nos includs das páginas

--- flask/server.py
+++ flask/server.py
@@ -17,6 +17,9 @@ def principal():
         return "Server not available"
     return render_template('index.html')
 
+@app.route('/ssh')
+def valida_ssh():
+    return render_template('ssh.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
