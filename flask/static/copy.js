c7f9555ec19500cc7a137288130161081c1a4e34
Gileno <gileno@tsmx.net.br>
2023-12-14 14:23:41 -0300

    feat(copy clipboard): adicionado funcao para copiar para o clipboard

--- flask/static/copy.js
+++ flask/static/copy.js
@@ -27,3 +27,24 @@ function copiarTexto() {
 
     
 }
+
+
+
+//FUNCAO UTILIZADA NO SGP
+<script>
+  function copyToClipboard(containerid) {
+    var range = document.createRange();
+    range.selectNode(containerid);
+
+    window.getSelection().removeAllRanges(); 
+    window.getSelection().addRange(range); 
+
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+
+    toastr.options['timeOut'] = '1500';
+    toastr["info"]("{% trans "Copiado!" %}");
+    toastr.options['timeOut'] = '6000000';
+    return false;
+  };
+</script>
