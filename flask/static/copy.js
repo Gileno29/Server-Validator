2419ace06d185a8f750f95589dacf5b1d4c7c2e2
Gileno <gileno@tsmx.net.br>
2023-12-14 17:49:23 -0300

    fix(copy to clipboar): copiar e colar funcional

--- flask/static/copy.js
+++ flask/static/copy.js
@@ -4,35 +4,8 @@ async function getPermissions() {
     return permissions
 }
 
-function copiarTexto() {
-    let textoCopiado = document.getElementsByClassName("texto-a-copiar");
-    let text=''
-    permissions=getPermissions()
-    console.log('This is my permissions: ', permissions)
-    if (permissions.includes("clipboard-write")) {
-        Array.prototype.forEach.call(textoCopiado, function(el) {
-            text= text+el.innerText+ "\n"
-            console.log(text)
-          });
-          navigator.clipboard.readText(text);
-          //textoCopiado.setSelectionRange(0, 99999)
-          //document.execCommand("copy");
-      
-          alert("O texto é: " + text);
-      } else {
-        console.log("Erro nas permissões")
-      }
-
-   
-
-    
-}
-
-
-
-//FUNCAO UTILIZADA NO SGP
-<script>
-  function copyToClipboard(containerid) {
+function copiarTexto(containerid) {
+ 
     var range = document.createRange();
     range.selectNode(containerid);
 
@@ -42,9 +15,9 @@ function copiarTexto() {
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
 
-    toastr.options['timeOut'] = '1500';
-    toastr["info"]("{% trans "Copiado!" %}");
-    toastr.options['timeOut'] = '6000000';
+    //toastr.options['timeOut'] = '1500';
+    //toastr["info"]("{% trans "Copiado!" %}");
+    //toastr.options['timeOut'] = '6000000';
     return false;
   };
-</script>
+    
